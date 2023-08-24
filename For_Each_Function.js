594176beeee4f57e523f199202586e2b4c725ab0
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-24 22:50:44 +0530

    Create For_Each_Function

---
+++ For_Each_Function.js
@@ -0,0 +1,20 @@
+// void type
+
+var arr = [10, 20, 30, 40, 50, 60];
+undefined
+arr.forEach(e=>console.log(e));
+VM1128:1 10
+VM1128:1 20
+VM1128:1 30
+VM1128:1 40
+VM1128:1 50
+VM1128:1 60
+undefined
+
+arr.forEach((element, index)=>console.log(index, element));
+VM1185:1 0 10
+VM1185:1 1 20
+VM1185:1 2 30
+VM1185:1 3 40
+VM1185:1 4 50
+VM1185:1 5 60
