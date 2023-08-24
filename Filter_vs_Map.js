29df9367c905b7d52c0133497a8e68287a4d8c66
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-24 22:42:42 +0530

    Update Filter_vs_Map

--- Filter_vs_Map.js
+++ Filter_vs_Map.js
@@ -21,3 +21,24 @@ var j = arr.map(e=>e*2);
 undefined
 j;
 (6) [20, 40, 60, 80, 100, 120]
+
+// more example
+
+var prices = [10000, 200000, 40000000, 6000.5];
+undefined
+prices;
+(4) [10000, 200000, 40000000, 6000.5]
+
+var p = 100000;
+undefined
+p.toLocaleString('hi');
+'1,00,000'
+p.toLocaleString('us');
+'100,000'
+p.toLocaleString('fr');
+'100 000'
+
+var formattedPrices = prices.map(p=>`Rs${p.toLocaleString('hi')}`);
+undefined
+formattedPrices;
+(4) ['Rs10,000', 'Rs2,00,000', 'Rs4,00,00,000', 'Rs6,000.5']
