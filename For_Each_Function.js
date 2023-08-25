519af9c824c4330a698581bd7943378e0a51a5d9
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-25 10:40:30 +0530

    Update For_Each_Function

--- For_Each_Function.js
+++ For_Each_Function.js
@@ -1,6 +1,6 @@
 // void type
-// It's a function and there is a loop going inside for traversal
-// In for_of we have to make it traverse.
+// It's a function and there is a loop going inside for traversal. It is Declarative Programming
+// In for_of we have to make it traverse. It is Imperative Programming.
 
 var arr = [10, 20, 30, 40, 50, 60];
 undefined
