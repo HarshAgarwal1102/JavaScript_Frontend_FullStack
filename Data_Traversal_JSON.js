7d68fdcbd8b13d56eef00d0cc4f83992ff381c4d
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-26 23:48:00 +0530

    Create Data_Traversal_JSON in JS

---
+++ Data_Traversal_JSON.js
@@ -0,0 +1,25 @@
+var emp = {id: 1001 , name: 'Rohit'};
+undefined
+var json = JSON.stringify(emp);
+undefined
+json;
+'{"id":1001,"name":"Rohit"}'
+typeof(json);
+'string'
+//Stringify converts all the data of the emp into String 
+undefined
+JSON.parse(json);
+{id: 1001, name: 'Rohit'}
+//JSON to object - method is parse
+//Object to JSON - method is stringify
+undefined
+var jsonn = JSON.stringify(emp);
+undefined
+jsonn;
+'{"id":1001,"name":"Rohit"}'
+JSOn.parse(jsonn);
+VM796:1 Uncaught ReferenceError: JSOn is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM796:1
+JSON.parse(jsonn);
+{id: 1001, name: 'Rohit'}
