d29702cdcd966e1a2ebf2eca721fc18959c885ae
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-30 22:24:10 +0530

    Update Memoization.js

--- Memoization.js
+++ Memoization.js
@@ -3,23 +3,27 @@
 function cube(num){
     for(let i = 1; i<=100000; i++){
         for(let j = 1; j<=10000; j++){
-            
+            // heavy-duty work as loops are too big.
         }
     }
     return num**3;
 }
 undefined
 
-cube(3);
+                     // PROBLEM
+// if the function does some heavy-duty work, then, computing the result again and again with the same parameter will lead to wastage of time.
+cube(3);     
 27
 Window {window: Window, self: Window, document: document, name: '', location: Location, …}
 
 
 // Good Solution
-// Use of cahching facility
 
-// we have created object of cache instead array, because object will complete the work in constant time and array will definately took some what n or logn time.
-var cache = {};  
+// This is where memoization comes in, by using memoization we can store(cache) the computed results based on the parameters. 
+// If the same parameter is used again while invoking the function, instead of computing the result, we directly return the stored (cached) value.
+
+
+var cache = {};  // we have created object of cache instead array, because object will complete the work in constant time and array will definately took some what n or logn time.
 function cube(num){
     if(cache[num]){
         return cache[num];
