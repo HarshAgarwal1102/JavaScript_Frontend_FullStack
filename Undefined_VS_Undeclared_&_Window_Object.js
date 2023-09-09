d757c496d98debfaecbbd8d29b9d59150802cb29
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-09 13:02:04 +0530

    Update Undefined_VS_Undeclared_&_Window_Object.js

--- Undefined_VS_Undeclared_&_Window_Object.js
+++ Undefined_VS_Undeclared_&_Window_Object.js
@@ -1,17 +1,20 @@
 // undefined vs undeclared
-var a ; // undefined
+
+
+var a ;            // undefined
 undefined
 a;
 undefined
 typeof a;
 'undefined'
 
-t; // undeclared
+t;                  // undeclared
 VM104:1 Uncaught ReferenceError: t is not defined
     at <anonymous>:1:1
 (anonymous) @ VM104:1
-r = 100;
-100
+
+r = 100;            // Accidental Global Variable
+100 
 r;
 100
 typeof r;
@@ -22,15 +25,18 @@ r;
 // Window Object
 window.r;
 100
-typeof window;
+typeof window;          // window is an object
 'object'
-window instanceof Window;
+window instanceof Window;         // Window is a function
 true
 typeof Window;
 'function'
+
 window ;
 Window {window: Window, self: Window, document: document, name: '', location: Location, …}
-this;
+
+this;           // this is reffering to current object i.e., window
 Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+
 window == this;
 true
