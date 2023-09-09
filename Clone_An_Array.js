f0c7c5293e1551c9b302858ec65b8711961f2773
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-09 19:09:10 +0530

    Update Clone_An_Array.js

--- Clone_An_Array.js
+++ Clone_An_Array.js
@@ -2,9 +2,9 @@
 
 //Using the spread operator we can make a clone of an array
 var arr = [10, 20, 30, 40, 50, 60];
-undefined
+
 var arr1 = [...arr];
-undefined
+
 arr1;
 (6) [10, 20, 30, 40, 50, 60]
 arr1 === arr
@@ -14,7 +14,7 @@ false
 //Without using the spread operator we can also make a clone of an array (using filter method)
 
 var g = arr.filter(w=>true); // we are creating a sub array
-undefined
+
 g;
 (6) [10, 20, 30, 40, 50, 60]
 g === arr1;
