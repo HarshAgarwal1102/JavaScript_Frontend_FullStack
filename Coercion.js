c12feb3d3a06d7be6a270e501242ed7ecee85568
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-09 13:45:36 +0530

    Update Coercion.js

--- Coercion.js
+++ Coercion.js
@@ -37,23 +37,26 @@ NaN
 ''
 
 
-100 + parseInt("100");
+
+// Explicit Coercion...
+
+
+100 + parseInt("100");          // We have converted it explicitly.. parseInt() is a function
 200
-100 - "100"
+100 - "100"        // this will work fine implicitly..
 0
-100 * "10"
+100 * "10"          // this will work fine implicitly..
 1000
-100 + parseFloat("100.20");
+100 + parseFloat("100.20");        // We have converted it explicitly..  parseFloat() is a function
 200.2
-"Amit" + String(2000);
+"Amit" + String(2000);      // We have converted it explicitly..
 'Amit2000'
-typeof String;
+typeof String;        String is a Function...
 'function'
 typeof parseInt;
 'function'
-100.toString();
-VM661:1 Uncaught SyntaxError: Invalid or unexpected token
+
 var e = 100;
 undefined
-e.toString();
+e.toString();   // another ques aries that, How we applied . on primitive type, we can use it only with refernce types..
 '100'
