3d5c1fe490dde4f72b16e736b8400ef81d0ea6d2
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-09 10:17:03 +0530

    Update and rename Declaration_and_Types_console.js to Declaration_and_TypeOf.js

---
+++ Declaration_and_TypeOf.js
@@ -0,0 +1,45 @@
+// JS is a dynamically typed programming language, which means that data types of variables are determined by the value they hold at runtime 
+// and can change throughout the program as we assign different values to them.
+
+var a;
+undefined
+typeof a;   // if no value is assigned to a variable then it's value and type must be undefined
+'undefined'
+a=100;
+100
+typeof a;
+'number'
+a= "Amit";
+'Amit'
+typeof a;
+'string'
+typeof (a);  // type of is not a function
+'string'
+typeof ((a));  // brackets are used for giving priority
+'string'
+typeof ((((a))));  
+'string'
+typeof typeof;  // it's like a keyword
+VM226:1 Uncaught SyntaxError: Unexpected token ';'
+a=true;
+true
+typeof a;  
+'boolean'
+a = "Amit";
+'Amit'
+typeof a;
+'string'
+typeof 10+20;
+'number20'
+typeof (10+20);
+'number'
+typeof undefined;
+'undefined'
+var x = 100;
+undefined
+var y = x;
+undefined
+y + 10;
+110
+y;
+100
