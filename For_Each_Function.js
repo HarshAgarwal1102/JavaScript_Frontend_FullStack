8757657dce4483a493e646eda591b90676edf546
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-09-09 19:19:46 +0530

    Update For_Each_Function.js

--- For_Each_Function.js
+++ For_Each_Function.js
@@ -3,20 +3,19 @@
 // In for_of we have to make it traverse. It is Imperative Programming.
 
 var arr = [10, 20, 30, 40, 50, 60];
-undefined
+
 arr.forEach(e=>console.log(e));
-VM1128:1 10
-VM1128:1 20
-VM1128:1 30
-VM1128:1 40
-VM1128:1 50
-VM1128:1 60
-undefined
+10
+20
+30
+40
+50
+60
 
 arr.forEach((element, index)=>console.log(index, element));
-VM1185:1 0 10
-VM1185:1 1 20
-VM1185:1 2 30
-VM1185:1 3 40
-VM1185:1 4 50
-VM1185:1 5 60
+0 10
+1 20
+2 30
+3 40
+4 50
+5 60
